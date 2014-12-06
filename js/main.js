a78b57ea4f7bed163b2449dab00d2efca476419c
ImanGhader <emanghader@gmail.com>
2014-12-05 20:18:17 -0500

    Fixed bugs ins parallax and viewport

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var $win = $(window);
-var $homeBanner = $('.slogan');
+var $homeBanner = $('.home-banner');
+var $workBanner = $('.work-banner');
+var workBannerOffset = $workBanner.offset();
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
+	var workPos = (scrollPos - workBannerOffset.top) / 2;
 
 	$homeBanner.css('background-position', 'center ' + scrollPos / 2 + 'px');
-
+	$workBanner.css('background-position', 'center ' + workPos + 'px');
 });
 
 $(window).load(function() {
