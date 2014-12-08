7af0975601be99638d9d71f5770b6769c0fbfcda
ImanGhader <emanghader@gmail.com>
2014-12-08 00:24:23 -0500

    Fixed paralax on Large screen and nav on all

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ var $win = $(window);
 var $homeBanner = $('.home-banner');
 var $workBanner = $('.work-banner');
 var workBannerOffset = $workBanner.offset();
+var $howBanner = $('.how-banner');
+var howBannerOffset = $howBanner.offset();
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
-	var workPos = (scrollPos - workBannerOffset.top) / 2;
+	var workPos = (scrollPos - workBannerOffset.top) / 4;
+	var howPos = (scrollPos - howBannerOffset.top) / 4;
 
 	$homeBanner.css('background-position', 'center ' + scrollPos / 2 + 'px');
 	$workBanner.css('background-position', 'center ' + workPos + 'px');
+	$howBanner.css('background-position', 'center ' + howPos + 'px');
 });
 
 $(window).load(function() {
