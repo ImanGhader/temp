d3e0512250e4a24042c34e843e8580ada11cba77
ImanGhader <emanghader@gmail.com>
2014-12-09 13:39:02 -0500

    Added all alt tags and changed hover state

---
+++ Iman Ghader · Graphic Designer · Ottawa, Canada_files/main.js
@@ -0,0 +1,27 @@
+var $win = $(window);
+var $homeBanner = $('.home-banner');
+var $workBanner = $('.work-banner');
+var workBannerOffset = $workBanner.offset();
+var $howBanner = $('.how-banner');
+var howBannerOffset = $howBanner.offset();
+
+
+if (window.screen.width >= 480) {
+
+	$win.on('scroll', function () {
+		
+		var scrollPos = $win.scrollTop();
+		var workPos = (scrollPos - workBannerOffset.top) / 4;
+		var howPos = (scrollPos - howBannerOffset.top) / 4;
+
+		$homeBanner.css('background-position', 'center ' + scrollPos / 2 + 'px');
+		$workBanner.css('background-position', 'center ' + workPos + 'px');
+		$howBanner.css('background-position', 'center ' + howPos + 'px');
+	});
+}
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
